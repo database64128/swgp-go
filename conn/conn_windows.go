a83aaf3de21496ac7d1f9665ea38ececec8edb86
database64128 <free122448@hotmail.com>
2023-03-28 23:24:34 +0800

    🗑️ conn: remove old ListenUDP function

--- conn/conn_windows.go
+++ conn/conn_windows.go
@@ -1,11 +1,8 @@
 package conn
 
 import (
-	"context"
 	"fmt"
-	"net"
 	"net/netip"
-	"syscall"
 	"unsafe"
 
 	"golang.org/x/sys/windows"
@@ -67,39 +64,6 @@ func (lso ListenerSocketOptions) buildSetFns() setFuncSlice {
 	return setFuncSlice{}.
 		appendSetPMTUDFunc(lso.PathMTUDiscovery).
 		appendSetRecvPktinfoFunc(lso.ReceivePacketInfo)
-}
-
-// ListenUDP wraps [net.ListenConfig.ListenPacket] and sets socket options on supported platforms.
-//
-// On Linux and Windows, IP_MTU_DISCOVER and IPV6_MTU_DISCOVER are set to IP_PMTUDISC_DO to disable IP fragmentation
-// and encourage correct MTU settings. If pktinfo is true, IP_PKTINFO and IPV6_RECVPKTINFO are set to 1.
-//
-// On Linux, SO_MARK is set to user-specified value.
-//
-// On macOS and FreeBSD, IP_DONTFRAG, IPV6_DONTFRAG are set to 1 (Don't Fragment).
-func ListenUDP(network string, laddr string, pktinfo bool, fwmark int) (*net.UDPConn, error) {
-	lc := net.ListenConfig{
-		Control: func(network, address string, c syscall.RawConn) (err error) {
-			if cerr := c.Control(func(fd uintptr) {
-				if err = setPMTUD(int(fd), network); err != nil {
-					return
-				}
-
-				if pktinfo {
-					err = setRecvPktinfo(int(fd), network)
-				}
-			}); cerr != nil {
-				return cerr
-			}
-			return
-		},
-	}
-
-	pc, err := lc.ListenPacket(context.Background(), network, laddr)
-	if err != nil {
-		return nil, err
-	}
-	return pc.(*net.UDPConn), nil
 }
 
 // Structure CMSGHDR from ws2def.h
