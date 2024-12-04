6930b2f6e148cbf497ffb0611298135a2bbbe4f0
database64128 <free122448@hotmail.com>
2024-12-04 11:16:07 +0800

    📮 cmd: add version flag
    Log name and version on startup.

--- cmd/swgp-go/main.go
+++ cmd/swgp-go/main.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"runtime/debug"
 	"syscall"
 
+	"github.com/database64128/swgp-go"
 	"github.com/database64128/swgp-go/jsonhelper"
 	"github.com/database64128/swgp-go/logging"
 	"github.com/database64128/swgp-go/service"
@@ -16,6 +18,7 @@ import (
 )
 
 var (
+	version  bool
 	testConf bool
 	confPath string
 	zapConf  string
@@ -23,6 +26,7 @@ var (
 )
 
 func init() {
+	flag.BoolVar(&version, "version", false, "Print the version and exit")
 	flag.BoolVar(&testConf, "testConf", false, "Test the configuration file and exit without starting the services")
 	flag.StringVar(&confPath, "confPath", "config.json", "Path to the JSON configuration file")
 	flag.StringVar(&zapConf, "zapConf", "console", "Preset name or path to the JSON configuration file for building the zap logger.\nAvailable presets: console, console-nocolor, console-notime, systemd, production, development")
@@ -32,12 +36,22 @@ func init() {
 func main() {
 	flag.Parse()
 
+	if version {
+		os.Stdout.WriteString("swgp-go\t" + swgp.Version + "\n")
+		if info, ok := debug.ReadBuildInfo(); ok {
+			os.Stdout.WriteString(info.String())
+		}
+		return
+	}
+
 	logger, err := logging.NewZapLogger(zapConf, logLevel)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to build logger:", err)
 		os.Exit(1)
 	}
 	defer logger.Sync()
+
+	logger.Info("swgp-go", zap.String("version", swgp.Version))
 
 	var sc service.Config
 	if err = jsonhelper.OpenAndDecodeDisallowUnknownFields(confPath, &sc); err != nil {
