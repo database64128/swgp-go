1e5ed2fd9b06f0c42464bc805922ae9fbae7cbb2
database64128 <free122448@hotmail.com>
2025-02-17 10:47:15 +0800

    🥕 slicehelper: prefer slices package
    slices.Grow is better because it exposes all additional capacity from rounding up to the next size class.

--- slicehelper/slicehelper.go
+++ slicehelper/slicehelper.go
@@ -1,15 +1,11 @@
 package slicehelper
 
+import "slices"
+
 // Extend extends the input slice by n elements. head is the full extended
 // slice, while tail is the appended part. If the original slice has sufficient
 // capacity no allocation is performed.
 func Extend[S ~[]E, E any](in S, n int) (head, tail S) {
-	if total := len(in) + n; cap(in) >= total {
-		head = in[:total]
-	} else {
-		head = make(S, total)
-		copy(head, in)
-	}
-	tail = head[len(in):]
-	return
+	head = slices.Grow(in, n)[:len(in)+n]
+	return head, head[len(in):]
 }
